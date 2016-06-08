ccef53cfc6b8cc18e9bee74822cb1aec772a65bf
ThomasBolander <privat@thomasbolander.dk>
2016-06-08 16:21:45 +0200

    Naming

--- Instagram.php
+++ Instagram.php
@@ -8,9 +8,9 @@ function getImagesByHashtag($hashtag = null, $count = 16)
     $hashtag = strtolower($hashtag);
     $parameters = urlencode("ig_hashtag($hashtag) { media.first($count) {   count,   nodes {     caption,     code,     comments {       count     },     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
     $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
-    $more = json_decode(file_get_contents($url));
-    $more = $more->media->nodes;
-    return $more;
+    $images = json_decode(file_get_contents($url));
+    $images = $images->media->nodes;
+    return $images;
 }
 
 function getImagesByUserID($user = null, $count = 16)
@@ -21,9 +21,9 @@ function getImagesByUserID($user = null, $count = 16)
 	}
     $parameters = urlencode("ig_user($user) { media.first($count) {   count,   nodes {     caption,     code,     comments {       count     },     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
     $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
-    $more = json_decode(file_get_contents($url));
-    $more = $more->media->nodes;
-    return $more;
+    $images = json_decode(file_get_contents($url));
+    $images = $images->media->nodes;
+    return $images;
 }
 
 ?>
