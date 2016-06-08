8ba5f764e08e41e8da371353bb4a3a14bc84abbd
Scott Kennedy <scottyzen@gmail.com>
2016-06-08 20:54:48 +0100

    Update example.php
    Display square images and put them into img tags

--- test/example.php
+++ test/example.php
@@ -8,3 +8,11 @@ print_r($media);
 //Its getting 10 images/videoes from Selena Gomezs instagram.. mmhh..
 $media = Bolandish\Instagram::getMediaByHashtag(460563723, 10);
 print_r($media);
+
+//Display square images only
+$media = Bolandish\Instagram::getMediaByHashtag("wildlife", 20);
+foreach($pics as $key=>$value){
+  if ($pics[$key]->dimensions->width === $pics[$key]->dimensions->height){
+    echo '<img src="'.$pics[$key]->display_src.'"/>';
+  }
+}
