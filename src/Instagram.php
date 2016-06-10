5c37fcb10d948f6b1cfef0ae68494e8b6fb76668
ThomasBolander <privat@thomasbolander.dk>
2016-06-10 13:04:31 +0200

    Comment count added fixes #12

--- src/Instagram.php
+++ src/Instagram.php
@@ -2,43 +2,60 @@
 namespace Bolandish;
 
 class Instagram {
-    public static function getMediaByHashtag($hashtag = null, $count = 16, $assoc = false)
+    public static function getMediaByHashtag($hashtag = null, $count = 16, $assoc = false, $comment_count = false)
     {
         if ( empty($hashtag) || !is_string($hashtag) )
         {
             return false;
         }
         $hashtag = strtolower($hashtag);
-        $parameters = urlencode("ig_hashtag($hashtag) { media.first($count) {   count,   nodes {     caption,     code,     comments {       count     },     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
+        $parameters = urlencode("ig_hashtag($hashtag) { media.first($count) {   count,   nodes {     caption,     code,    comments.last($comment_count) {           count,           nodes {             id,             created_at,             text,             user {               id,               profile_pic_url,               username             }           },           page_info         },     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
         $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
         $media = json_decode(file_get_contents($url), ($assoc || $assoc == "array"));
-        $media = $media->media->nodes;
+        if($assoc == "array")
+            $media = $media["media"]["nodes"];
+        else
+            $media = $media->media->nodes;
         return $media;
     }
 
-    public static function getMediaByUserID($user = null, $count = 16, $assoc = false)
+    public static function getMediaByUserID($user = null, $count = 16, $assoc = false, $comment_count = false)
     {
         if ( empty($user) || !(is_string($user) || is_int($user)) )
         {
             return false;
         }
-        $parameters = urlencode("ig_user($user) { media.first($count) {   count,   nodes {     caption,     code,     comments {       count     },     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
+        $parameters = urlencode("ig_user($user) { media.first($count) {   count,   nodes {     caption,     code,     comments.last($comment_count) {           count,           nodes {             id,             created_at,             text,             user {               id,               profile_pic_url,               username             }           },           page_info         },     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
         $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
         $media = json_decode(file_get_contents($url),($assoc || $assoc == "array"));
-        $media = $media->media->nodes;
+        if($assoc == "array")
+            $media = $media["media"]["nodes"];
+        else
+            $media = $media->media->nodes;
         return $media;
     }
 
-    public static function getMediaAfterByUserID($user = null, $min_id, $count = 16, $assoc = false)
+    public static function getMediaAfterByUserID($user = null, $min_id, $count = 16, $assoc = false, $comment_count = false)
     {
         if ( empty($user) || !(is_string($user) || is_int($user)) )
         {
             return false;
         }
-        $parameters = urlencode("ig_user($user) { media.after($min_id,$count) {   count,   nodes {     caption,     code,     comments {       count     },     date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
+        if($comment_count){
+            $comments = "comments.last($comment_count) {           count,           nodes {             id,             created_at,             text,             user {               id,               profile_pic_url,               username             }           },           page_info         }";
+        }else{
+            $comments = "comments {       count     }";
+        }
+
+        $parameters = urlencode("ig_user($user) { media.after($min_id,$count) {   count,   nodes {     caption,     code,    $comments,   date,     dimensions {       height,       width     },     display_src,     id,     is_video,     likes {       count     },     owner {       id,       username,       full_name,       profile_pic_url,     biography     },     thumbnail_src,     video_views,     video_url   },   page_info }  }");
+
         $url = "https://www.instagram.com/query/?q=$parameters&ref=tags%3A%3Ashow";
         $media = json_decode(file_get_contents($url),($assoc || $assoc == "array"));
-        $media = $media->media->nodes;
+        if($assoc == "array")
+            $media = $media["media"]["nodes"];
+        else
+            $media = $media->media->nodes;
+
         return $media;
     }
 }
--- test/example.php
+++ test/example.php
@@ -3,15 +3,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 //Its getting 10 images/videoes from instagram with the hastag #nofilter
 $media = Bolandish\Instagram::getMediaByHashtag("nofilter", 10);
-print_r($media);
 
 //Its getting 10 images/videoes from Selena Gomezs instagram.. mmhh..
-$media = Bolandish\Instagram::getMediaByHashtag(460563723, 10);
-print_r($media);
+$media = Bolandish\Instagram::getMediaByUserID(460563723, 10);
 
 //Get 10 images/videos after the media with the id 1060728019300790746 from Selena Gomex
-$media = Bolandish\Instagram::getMediaAfterByHashtag(460563723,1060728019300790746, 10);
-
+$media = Bolandish\Instagram::getMediaAfterByUserID(460563723,1060728019300790746, 10);
 
 //Display square images only
 $media = Bolandish\Instagram::getMediaByHashtag("wildlife", 20);
@@ -20,10 +17,9 @@ foreach($media as $value){
     echo '<img src="' .$value->display_src. '" alt="' .$value->caption. '" />';
   }
 }
-
 //Make images clickable and link to instagram post
 foreach($media as $key=>$value){
-  if ($media[$key]->dimensions->width === $media[$key]->dimensions->height){
+  if ($value->dimensions->width === $value->dimensions->height){
 		echo '<a href="'.'https://www.instagram.com/p/'.$media[$key]->code.'" target="_blank"><img src="'.$media[$key]->display_src.'" alt="'.$media[$key]->caption.'" /></a>';
   }
 }
